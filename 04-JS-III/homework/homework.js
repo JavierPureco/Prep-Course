21f410da4311414555f21ef4c62cda0e71126484
Francisco JAvier <fco.pureco@gmail.com>
2022-12-13 23:59:52 -0600

    third test

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -167,7 +167,10 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  var text = n.toString()
+  if (text.charAt(0) === "9"){
+    return true
+  } return false
 }
 
 
@@ -175,7 +178,11 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+  for (var i = 0; i<arreglo.length;i++){
+    if( arreglo[i] === arreglo[i+1]){
+      return true
+    } return false
+  }
 } 
 
 
@@ -184,6 +191,17 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  let array2=[]
+ for (var i= 0; i<array.length;i++){
+  if (array[i]==="Enero" || array[i]==="Marzo" || array[i]==="Noviembre" ){
+     array2.push(array[i])
+  } 
+ } if (array2.length < 3){
+  /// este aplica para saber si encontramos los 3
+  return "No se encontraron los meses pedidos"
+ }
+  return array2
+ 
 }
 
 
@@ -191,6 +209,12 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+let arrayMayoresA100 = []
+for (let i= 0; i < array.length; i++){
+  if (array[i]>100){
+    arrayMayoresA100.push(array[i])
+  }
+} return arrayMayoresA100
 }
 
 
@@ -202,6 +226,19 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  let sumaDos = numero 
+  let array=[]
+
+for (var i=0; i<10 ; i++ ){ /// usar var por que let es solo dentro
+  sumaDos = sumaDos + 2
+  if (sumaDos===i) break
+  else {
+    array.push(sumaDos)
+  }
+}
+if (i<10){
+  return "Se interrumpió la ejecución"
+} return array
 }
 
 
@@ -212,6 +249,20 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+var array = []
+var suma = numero
+
+for (var i= 0; i<10; i++){
+  if (i===5) continue
+  else {
+    suma=suma+2
+    array.push(suma)
+  }
+}
+  
+return array
+
+
 }
 
 
